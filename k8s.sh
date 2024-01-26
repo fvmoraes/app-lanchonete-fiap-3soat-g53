a8282a67c670a4f7aff65cb8d5218cd01140799c
Franklin Moraes <fvmoraes@fvmoraes-macbook.local>
2024-01-26 18:47:05 -0300

    add: documentação

--- k8s.sh
+++ k8s.sh
@@ -3,4 +3,5 @@ docker build -f docker/Dockerfile -t fvmoraes/applanchonetetok8s:latest .
 docker login
 docker push fvmoraes/applanchonetetok8s:latest
 minikube start
+kubectl apply -f k8s/namespace.yaml
 kubectl apply -k k8s
