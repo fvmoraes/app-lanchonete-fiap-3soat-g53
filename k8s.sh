14c5cac9021ddefb3a67e6db942f8bef83de286a
Franklin Moraes <fvmoraes@fvmoraes-macbook.local>
2024-01-26 21:15:06 -0300

    add: documentação

--- k8s.sh
+++ k8s.sh
@@ -5,5 +5,6 @@ docker push fvmoraes/applanchonetetok8s:latest
 minikube start
 kubectl apply -f k8s/namespace.yaml
 kubectl apply -k k8s
-kubectl port-forward svc/pgadmin-service 54321:54321 -n 3soatg53
-kubectl port-forward svc/applanchonete-service 8080:8080 -n 3soatg53
+
+kubectl port-forward svc/pgadmin-service 54321:80 -n 3soatg53 &
+kubectl port-forward svc/applanchonete-service 8080:8080 -n 3soatg53 &
